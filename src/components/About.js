3c38667b34f7ef9cbc4b930f881cf8fdc36ffa70
SHEZ <shezcode@gmail.com>
2023-01-11 14:30:43 +0100

    finished index page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import about from '../assets/about.png'
+import mario from '../assets/mario.jpg'
+import adrian from '../assets/adrian.jpg'
 import '../styles/About.css'
 
 const About = () => {
@@ -9,7 +10,7 @@ const About = () => {
       <h2 className='section-title primary-green'>Little Lemon</h2>
       <div className='desc-pics'>
         <p className='lead-text about-text'> We are Adrian and Mario, the founders of the Little Lemon. We started this journey back in 2005 when we saw the opportunity to start a great restaurant in downtown Chicago. Ever since, we have prepared thousands of delicious meals for clients from all over the world. We are so grateful for it!</p>
-        <img src={about} alt="" className='about-img'/>
+        <img src={adrian} alt="" className='about-img'/>
       </div>
 
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../styles/Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <h3>Doormat</h3>
-        <ul>
+    <footer className='foot'>
+      <div className='col1'>
+        <h3 className='section-title primary-yellow'>Little Lemon</h3>
+        <ul className='lil'>
           <li><a href="/home">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/menu">Menu</a></li>
@@ -14,17 +15,17 @@ const Footer = () => {
           <li><a href="/login">Login</a></li>
         </ul>
       </div>
-      <div>
-        <h3>Contact</h3>
-        <ul>
+      <div className='col2'>
+        <h3 className='section-title primary-yellow'>Contact</h3>
+        <ul className='lil'>
           <li><a href="/phone">Phone</a></li>
           <li><a href="/email">Email</a></li>
           <li><a href="/address">Address</a></li>
         </ul>
       </div>
-      <div>
-        <h3>Follow Us</h3>
-        <ul>
+      <div className='col3'>
+        <h3 className='section-title primary-yellow'>Follow Us</h3>
+        <ul className='lil'>
           <li><a href="/facebook">Facebook</a></li>
           <li><a href="/twitter">Twitter</a></li>
           <li><a href="/instagram">Instagram</a></li>
