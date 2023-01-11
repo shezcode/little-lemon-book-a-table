5d527fde24a6b50d79968d62b0888094da6acedc
SHEZ <shezcode@gmail.com>
2023-01-11 14:03:46 +0100

    more styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import about from '../assets/about.png'
+import '../styles/About.css'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='about-container'>
+      <h1 className='subtitle'>About Us</h1>
+      <h2 className='section-title primary-green'>Little Lemon</h2>
+      <div className='desc-pics'>
+        <p className='lead-text about-text'> We are Adrian and Mario, the founders of the Little Lemon. We started this journey back in 2005 when we saw the opportunity to start a great restaurant in downtown Chicago. Ever since, we have prepared thousands of delicious meals for clients from all over the world. We are so grateful for it!</p>
+        <img src={about} alt="" className='about-img'/>
+      </div>
+
     </div>
   )
 }
